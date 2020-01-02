0baf134d022fc4d8d02fc856f461a094c6863cdd
Yi <pingtaoyi@qq.com>
2020-01-02 23:46:20 +0800

    dbplyr + database

--- 数据库及RQLite.R
+++ 数据库及RQLite.R
@@ -1,3 +1,4 @@
+#----RSQLite
 library(RSQLite)
 #构建表格
 db<- dbConnect(SQLite(), dbname = 'Test.sqlite')
@@ -39,4 +40,81 @@ dbWriteTable(conn = db.hotcities,name = 'Hotcity',value = dt[1:10000,],overwrite
 tmd <- dbReadTable(db.hotcities,'Hotcity')
 head(tmd)
 dbDisconnect(db.hotcities)
+
+#dbplyr与数据库
+### 以下为内存中快速建立临时数据库
+iris2 <- dbplyr::src_memdb() %>% copy_to(iris, overwrite = TRUE)
+iris2 %>% class()
+
+### 以下为用dbplyr操纵sqllite数据库
+con <- dbConnect(RSQLite::SQLite(),dbname = 'db_yi')
+dplyr::copy_to(con,nycflights13::flights,'flight')
+dbReadTable(con,'flight')
+flights_db <- tbl(con,'flight')
+class(flights_db)
+flights_db
+flights_db %>% select(year:day,arr_time)
+flights_db %>% filter(dep_delay > 240) %>% show_query()
+flights_db %>% 
+  group_by(dest) %>%
+  summarise(delay = mean(dep_time)) %>% show_query()
+
+tailnum_delay_db <- flights_db %>% 
+  group_by(tailnum) %>%
+  summarise(
+    delay = mean(arr_delay),
+    n = n()
+  ) %>% 
+  arrange(desc(delay)) %>%
+  filter(n > 100)
+
+class(tailnum_delay_db)
+tailnum_delay_db %>% show_query()
+tail(tailnum_delay_db) # 因为tailnum_delay_db是lazzy属性，tail不能配合使用
+
+tailnum_delay <- tailnum_delay_db %>% collect() #实现向R中tibble的转换
+
+tailnum_delay %>% tail()
+
+dbDisconnect(con)
+
+### 以下为用dbplyr操纵postgreSQL数据库
+####1.写入数据，再操作
+m <- RPostgreSQL::PostgreSQL() # m <- DBI::dbDriver('PostgreSQL') #效果相同
+con <- dbConnect(m,user = 'postgres',password = '',dbname = 'postgres')
+#与下句同作用
+# con <- dbConnect(RPostgreSQL::PostgreSQL(),user = 'postgres',password = '',dbname = 'postgres')
+dplyr::copy_to(con,nycflights13::flights,'flight',temporary = F,overwrite = T)
+dbReadTable(con,'flight')
+flights_db <- tbl(con,'flight')
+class(flights_db)
+flights_db
+flights_db %>% select(year:day,arr_time)
+flights_db %>% filter(dep_delay > 240) %>% show_query()
+flights_db %>% 
+  group_by(dest) %>%
+  summarise(delay = mean(dep_time)) %>% show_query()
+
+tailnum_delay_db <- flights_db %>% 
+  group_by(tailnum) %>%
+  summarise(
+    delay = mean(arr_delay),
+    n = n()
+  ) %>% 
+  arrange(desc(delay)) %>%
+  filter(n > 100)
+
+class(tailnum_delay_db)
+tailnum_delay_db %>% show_query()
+tail(tailnum_delay_db) # 因为tailnum_delay_db是lazzy属性，tail不能配合使用
+
+tailnum_delay <- tailnum_delay_db %>% collect() #实现向R中tibble的转换
+
+tailnum_delay %>% tail()
+
+dbDisconnect(con)
+####2.操作已存在的表
+con <- dbConnect(m,user = 'postgres', password = '',dbname = 'Yi')
+#dbReadTable(con,'Employment-Northeast(1)')
+emp <- tbl(con,'Employment-Northeast(1)',encoding = 'UTF-8')
 
